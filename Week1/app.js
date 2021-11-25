0b01f221227eb0b71354f2e60817e5231953fb7f
Micah <Micah.Ross@bryanuniversity.edu>
2021-11-24 18:55:53 -0600

    Node Module Basics and Patterns

---
+++ Week1/app.js
@@ -0,0 +1,7 @@
+const OperationsFx = require('./math');
+console.log(OperationsFx)
+
+console.log('10 + 15 = ' + OperationsFx.fAddition(10, 15));
+console.log('15 - 10 = ' + OperationsFx.fSubtract(15, 10));
+console.log('10 x 5 = ' + OperationsFx.fMultiply(10, 5));
+console.log('50 / 5 = ' + OperationsFx.fDivide(50, 5));
---
+++ Week1/math.js
@@ -0,0 +1,14 @@
+const fAddition = (a, b) =>{
+    return (a + b);
+};
+const fSubtract = (c, d) =>{
+    return c - d;
+};
+const fMultiply = (e, f) =>{
+    return e * f;
+};
+const fDivide = (g, h) =>{
+    return g / h;
+};
+
+module.exports = {fAddition, fSubtract, fMultiply, fDivide};
